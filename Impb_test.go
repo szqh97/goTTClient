db353fc0506cf7956263606c369ad8c97557a96e
yun <liyun@idongjia.cn>
2016-11-14 22:10:50 +0800

    add Basedefine and Message pb

---
+++ Impb_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/szqh97/goTTClient/IM_Login"
+	"testing"
+)
+
+func TestLogin(t *testing.T) {
+	loginreq := IM_Login.IMLoginReq{}
+	fmt.Println(loginreq)
+
+}
