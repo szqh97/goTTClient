9ee7a1f28ed99754aa9d90561b8b9b404169a355
yun <liyun@idongjia.cn>
2016-11-17 09:20:02 +0800

    update test

--- Impb_test.go
+++ Impb_test.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/szqh97/goTTClient/IM_BaseDefine"
 	"github.com/szqh97/goTTClient/IM_Login"
 	"testing"
 )
 
 func TestLogin(t *testing.T) {
 	loginreq := IM_Login.IMLoginReq{}
-	fmt.Println(loginreq)
+	username := "dj352801"
+	password := "111111"
+	version := "1.0.0"
+	client_type := IM_BaseDefine.ClientType_CLIENT_TYPE_ANDROID
+
+	loginreq.UserName = &username
+	loginreq.Password = &password
+	loginreq.ClientVersion = &version
+	loginreq.ClientType = &client_type
+	fmt.Println(loginreq.String())
+	fmt.Printf("%s", loginreq)
+	data, _ := proto.Marshal(&loginreq)
+	fmt.Printf("%s", data)
 
 }
